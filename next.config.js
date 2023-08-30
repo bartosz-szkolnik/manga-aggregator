fc36f06bb5a44b215b0103188d884f905889c9c0
Bartosz Szkolnik <bartosz.szkolnik@protonmail.com>
2023-08-30 16:48:21 +0200

    Remove unnecessary components

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   experimental: {
     serverActions: true,
+    logging: 'verbose',
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,44 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import Link from 'next/link';
+import LogoutButton from '../components/LogoutButton';
+
+export const dynamic = 'force-dynamic';
+
+export default async function Index() {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  const { data } = await supabase.from('manga').select('*');
+
+  return (
+    <div className="w-full flex flex-col items-center">
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm text-foreground">
+          <div />
+          <div>
+            {user ? (
+              <div className="flex items-center gap-4">
+                Hey, {user.email}!
+                <LogoutButton />
+              </div>
+            ) : (
+              <Link
+                href="/login"
+                className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+              >
+                Login
+              </Link>
+            )}
+          </div>
+        </div>
+      </nav>
+
+      <pre>
+        <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+      </pre>
+    </div>
+  );
+}
