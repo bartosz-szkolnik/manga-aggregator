68787f4af3c9afe30ed0eaf800347a784c11b52c
Bartosz Szkolnik <bartosz.szkolnik@protonmail.com>
2024-07-22 21:47:25 +0200

    Add confirm dialog to sign out button

--- src/components/auth/sign-out-button.tsx
+++ src/components/auth/sign-out-button.tsx
@@ -1,11 +1,32 @@
+'use client';
+
 import { Button } from '@components/ui/button';
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@components/ui/dialog';
+import { useState } from 'react';
 
 export function SignOutButton({ className }: { className?: string }) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <form action="/auth/sign-out" method="post">
-      <Button className={className} variant="ghost">
-        Sign out
-      </Button>
-    </form>
+    <Dialog open={open} onOpenChange={value => setOpen(value)}>
+      <DialogTrigger asChild>
+        <Button className={className} variant="ghost">
+          Sign out
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="mb-4">Are you sure you want to sign out?</DialogTitle>
+        </DialogHeader>
+        <DialogFooter>
+          <Button onClick={() => setOpen(false)} variant={'secondary'}>
+            Stay in the app
+          </Button>
+          <form action="/auth/sign-out" method="post">
+            <Button>Sign me out</Button>
+          </form>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
