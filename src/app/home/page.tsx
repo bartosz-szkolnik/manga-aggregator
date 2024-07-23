55f7b69915b1e362a6718a45ab153e66a73dd1ae
Bartosz Szkolnik <bartosz.szkolnik@protonmail.com>
2024-07-23 11:27:51 +0200

    Add query params to tabs

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,5 @@
 import { createServerClient } from '@utils/supabase/server';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@components/ui/tabs';
+import { TabsContent, TabsList, TabsTrigger } from '@components/ui/tabs';
 import { AddMangaDialog } from '@lib/add-manga/add-manga-dialog';
 import { AllMangas } from './components/all-mangas';
 import { UpdatedForYou } from './components/updated-for-you';
@@ -7,6 +7,7 @@ import { CurrentlyReading } from './components/currently-reading';
 import { Metadata } from 'next';
 import { Link } from '@components/ui/link';
 import { Button } from '@components/ui/button';
+import { QueryParamsTabs } from '@components/ui/query-params-tab';
 
 export const metadata: Metadata = {
   title: 'Home · Manga Aggregator',
@@ -29,7 +30,7 @@ export default async function HomePage() {
 
   return (
     <main className="h-full p-4 py-6 lg:p-8">
-      <Tabs defaultValue={isLoggedIn ? 'updated' : 'show-all'} className="h-full space-y-6">
+      <QueryParamsTabs defaultValue={isLoggedIn ? 'updated' : 'show-all'} className="h-full space-y-6">
         <div className="space-between flex items-center">
           <TabsList>
             {isLoggedIn && <TabsTrigger value="updated">Updated For You</TabsTrigger>}
@@ -37,6 +38,7 @@ export default async function HomePage() {
             <TabsTrigger value="in-library" disabled>
               In Your Library
             </TabsTrigger>
+            <TabsTrigger value="all-available-manga">All Available Manga</TabsTrigger>
           </TabsList>
           <div className="ml-auto mr-4">
             <AddMangaDialog />
@@ -50,7 +52,10 @@ export default async function HomePage() {
         </TabsContent>
         <TabsContent value="in-library">Show me manga from my library.</TabsContent>
         <TabsContent value="show-all">Show me all manga.</TabsContent>
-      </Tabs>
+        <TabsContent value="all-available-manga">
+          <AllMangas />
+        </TabsContent>
+      </QueryParamsTabs>
     </main>
   );
 }
