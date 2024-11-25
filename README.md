7dfae1be5ed509e09485996374e5989252d7a18a
Bartosz Szkolnik <bartosz.szkolnik@protonmail.com>
2024-11-25 21:22:56 +0100

    Prettify entire codebase

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ This little app was built to check and notify me (and you) when some new chapter
 
 Build with Next.js, React, TypeScript, Tailwind and Shadcn/ui.
 
-
 You can try out the current version [here](https://manga-aggregator.vercel.app/all-manga)
+
 <!-- TODO add image of how the app looks -->
 
 ## How it works:
--- src/app/images/[manga-id]/[manga-cover]/route.ts
+++ src/app/images/[manga-id]/[manga-cover]/route.ts
@@ -1,6 +1,6 @@
 export async function GET(
   _request: Request,
-  props: { params: Promise<{ 'manga-id': string; 'manga-cover': string }> }
+  props: { params: Promise<{ 'manga-id': string; 'manga-cover': string }> },
 ) {
   const params = await props.params;
   return await fetch(`https://mangadex.org/covers/${params['manga-id']}/${params['manga-cover']}`, {
