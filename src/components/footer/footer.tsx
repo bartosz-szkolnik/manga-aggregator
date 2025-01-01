eac451b96cf57ddbb46252c1a3e682568341cfda
Bartosz Szkolnik <bartosz.szkolnik@protonmail.com>
2025-01-01 17:16:36 +0100

    Happy New Year!

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,11 +3,11 @@ import { Link } from '@components/ui/link';
 export function Footer() {
   return (
     <p className="w-full truncate text-sm text-muted-foreground">
-      &copy; {new Date().getFullYear()} Bartosz Szkolnik Software.
+      &copy; 2024 - {new Date().getFullYear()} Bartosz Szkolnik Software.
       <br />
       All rights reserved.
       <br />
-      Build by Bartosz Szkolnik
+      Built by Bartosz Szkolnik
       <br />
       Manga&apos;s data is taken from{' '}
       <Link className="underline" href="https://mangadex.org/" target="_blank" rel="noopener noreferrer">
