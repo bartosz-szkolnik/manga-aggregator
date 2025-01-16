7f86b5d6aeb40ff6b959f9a27df0f1ab2e8346fa
Bartosz Szkolnik <bartosz.szkolnik@protonmail.com>
2025-01-16 17:26:26 +0100

    Improve not-found component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,13 +6,16 @@ export const metadata: Metadata = { title: 'Page not found' };
 
 export default function NotFound() {
   return (
-    // calc is used to circumvent the margin not working properly with h-screen
-    <div className="m-3 flex h-[calc(100vh-1.5rem)] w-screen flex-col items-center justify-center rounded-md bg-background shadow-lg shadow-slate-400 dark:shadow-none md:m-3">
-      <h2 className="text-2xl font-semibold tracking-tight">404 · Not Found</h2>
-      <p>This page doesn’t exist. </p>
-      <Button className="mt-4" asChild>
-        <Link href="/">Return Home</Link>
-      </Button>
+    <div className="flex max-h-screen min-h-screen md:bg-gradient-to-r md:from-gradient-from md:to-gradient-to">
+      <div className="max-h-screen w-full rounded-md bg-background shadow-lg shadow-slate-400 dark:shadow-none md:m-3">
+        <div className="ml-4 mt-4 flex h-[calc(100vh-2rem)] flex-col items-center justify-center">
+          <h2 className="text-2xl font-semibold tracking-tight">404 · Not Found</h2>
+          <p>This page doesn’t exist. </p>
+          <Button className="mt-4" asChild>
+            <Link href="/">Return Home</Link>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
